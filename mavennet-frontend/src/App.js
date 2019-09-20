f30cd44b3025c66ebb5aa193bb0160f206cfa7cf
Esdras Chaves <esdraslchaves@gmail.com>
2019-09-20 14:09:59 -0300

    testing auth methods

--- mavennet-frontend/src/App.js
+++ mavennet-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom'
 
 import UsersPage from './pages/UsersPage/UsersPage'
@@ -6,12 +6,52 @@ import AlbumsPage from './pages/AlbumsPage/AlbumsPage'
 import PhotosPage from './pages/PhotosPage/PhotosPage'
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
 
+import { login, logout, isAuthenticated, getLoggedUser } from './services/auth'
+
 import './App.css';
 
 
 function App() {
+
+  const [ currentUser, setCurrentUser ] = useState(null)
+
+  useEffect(() => {
+    if (isAuthenticated()) {
+      const currentUser = getLoggedUser()
+      handleLogin(currentUser)
+    }
+  }, [])
+
+  const handleLogin = user => {
+    if (user) {
+      console.log('login')
+      login(user)
+      setCurrentUser(user)
+    }
+  }
+
+  const handleLogout = () => {
+    logout()
+    setCurrentUser(null)
+  }
+
   return (
     <div className="App">
+
+      { currentUser ? 
+        (<button onClick={e => {
+          e.preventDefault()
+          handleLogout()
+        }}>Logout</button>)
+        : (<button onClick={ e =>{
+          e.preventDefault()
+          handleLogin({
+            name: "Esdras"
+          })
+        }}>Login</button>)
+      }
+
+      <div>{JSON.stringify(currentUser)}</div>
 
       <div className="app__main-content">
         <Switch>
--- mavennet-frontend/src/services/auth.js
+++ mavennet-frontend/src/services/auth.js
@@ -2,7 +2,7 @@ const LOCAL_STORAGE_KEY="my_secure_key"
 
 export const isAuthenticated = () => localStorage.getItem(LOCAL_STORAGE_KEY) !== null
 
-export const getLoggerUser = () => JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+export const getLoggedUser = () => JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
 
 export const login = user => localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(user))
 
